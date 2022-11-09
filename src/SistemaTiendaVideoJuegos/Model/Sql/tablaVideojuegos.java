c8f8fcd07eaaa50dd9fb37315544347a5a358510
RamonCheno <francisco.cheno.ocano@gmail.com>
2022-11-08 22:38:23 -0700

    Mostrar Datos Consulta Select

--- src/SistemaTiendaVideoJuegos/Model/Sql/tablaVideojuegos.java
+++ src/SistemaTiendaVideoJuegos/Model/Sql/tablaVideojuegos.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import javax.swing.JComboBox;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -29,7 +29,7 @@ public class tablaVideojuegos extends ConnectionMySQL {
         try {
             sql = "Insert into bd_tienda.videojuegos(Nombre, Genero,"
                     + "fechaLanzamiento, plataforma, numVentas, "
-                    + "empresaDev_id_empresaDev) Values (?,?,?,?,?,?);";
+                    + "empresaDev_id_empresaDev, empresaEditora_id_empresaEditora) Values (?,?,?,?,?,?,?);";
             ps = con.prepareStatement(sql);
             ps.setString(1, pro.getNombreVideojuego());
             ps.setString(2, pro.getGeneroVideojuego());
@@ -78,7 +78,9 @@ public class tablaVideojuegos extends ConnectionMySQL {
     }
     
     public boolean registrarEmpresaEdit(Productos pro){
+        
         PreparedStatement ps = null;
+        
         Connection con = getConexion();
         String sql = "Insert into bd_tienda.empresaeditora(nombreEmpresaEditora) Value(?);";
         
@@ -87,6 +89,7 @@ public class tablaVideojuegos extends ConnectionMySQL {
             
             ps.setString(1, pro.getEmpresaEditor());
             ps.execute();
+            
             return true;
          } catch (SQLException e) {
             System.err.println(e);
@@ -110,7 +113,7 @@ public class tablaVideojuegos extends ConnectionMySQL {
         ResultSet result = null;
         
         try {
-            sql = "Select nombreDeveloper from bd_tienda.empresadev order by nombreDeveloper ASC";
+            sql = "Select nombreDeveloper from bd_tienda.empresadev";
             ps = con.prepareStatement(sql);
             result = ps.executeQuery();
             while(result.next()==true){
@@ -132,4 +135,85 @@ public class tablaVideojuegos extends ConnectionMySQL {
         return empresaDevList;
     }
     
+    public ArrayList listaEmpresaEdit(){
+        ArrayList empresaEditList = new ArrayList<>();
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+        String sql;
+        ResultSet result = null;
+        try {
+            sql = "Select nombreEmpresaEditora from bd_tienda.empresaeditora";
+            ps = con.prepareStatement(sql);
+            result = ps.executeQuery();
+            while(result.next() == true){
+                productD = new Productos();
+                productD.setEmpresaEditor(result.getString("nombreEmpresaEditora"));
+                empresaEditList.add(productD);
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        finally{
+            try {
+                con.close();
+                
+            } catch (SQLException ex) {
+                System.err.println(ex);
+            }
+        }
+        return empresaEditList;
+    }
+    
+    public ArrayList<Productos> mostrarVideojuego(){
+        /*
+            select v.Id_videojuegos, v.Nombre, v.Genero, v.fechaLanzamiento, v.plataforma, v.numVentas, dev.nombreDeveloper, 
+            edit.nombreEmpresaEditora
+            from videojuegos as v
+            inner join empresadev as dev on v.empresaDev_id_empresaDev = dev.id_empresaDev 
+            inner join empresaeditora as edit on v.empresaEditora_id_empresaEditora = edit.id_empresaEditora;
+        */
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+        String sql;
+        ResultSet rs = null;
+        ArrayList<Productos> lista = new ArrayList<>();
+            
+        try {
+            sql= """
+                 select v.Id_videojuegos, v.Nombre, v.Genero, v.fechaLanzamiento, v.plataforma, v.numVentas, dev.nombreDeveloper, 
+                    edit.nombreEmpresaEditora
+                from videojuegos as v
+                inner join empresadev as dev on v.empresaDev_id_empresaDev = dev.id_empresaDev 
+                inner join empresaeditora as edit on v.empresaEditora_id_empresaEditora = edit.id_empresaEditora
+                order by v.Id_videojuegos asc
+                 """;
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                productD = new Productos();
+                productD.setIdVideojuego(rs.getInt("v.Id_videojuegos"));
+                productD.setNombreVideojuego(rs.getString("v.Nombre"));
+                productD.setGeneroVideojuego(rs.getString("v.Genero"));
+                productD.setFechaLanzamiento(rs.getDate("v.fechaLanzamiento"));
+                productD.setPlataforma(rs.getString("v.plataforma"));
+                productD.setNumVentas(rs.getInt("v.numVentas"));
+                productD.setEmpresaDev(rs.getString("dev.nombreDeveloper"));
+                productD.setEmpresaEditor(rs.getString("edit.nombreEmpresaEditora"));
+                lista.add(productD);
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        finally{
+            try {
+                con.close();
+                ps.close();
+                rs.close();
+                
+            } catch (SQLException ex) {
+                System.err.println(ex);
+            }
+        }
+        return lista;
+    }
 }
